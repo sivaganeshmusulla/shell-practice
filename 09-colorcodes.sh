296283ea6f43086e3ad23cb6c07dff2e112abd1d
sivaganeshmusulla <siva.musulla@gmail.com>
2026-08-13 22:21:17 +0530

    color codes 01

--- 09-colorcodes.sh
+++ 09-colorcodes.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 R="\e[31m"
 G="\e[32m"
-
+Y="\e[33m"
+B="\e[34m"
 #it will print the output "Hello world" to the console
-echo -e "${G}Hello world${R} it will print the output ${G}Hello world${R} to the console"
+echo -e "${R}Hello world it will print the output Hello world to the console"
+echo -e "${G}Hello world it will print the output Hello world to the console"
+echo -e "${Y}Hello world it will print the output ${Y}Hello world to the console"
